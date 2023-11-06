3a56388f5b9d69a67445647360cd7f44e864a1be
misumisumi <dragon511southern@gmail.com>
2023-11-06 15:13:47 +0900

    init: nixのpythonプロジェクトテンプレートの作成

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Playbooks for apps of my k8s cluster";
+  inputs.devenv.url = "github:cachix/devenv";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.nixpkgs-python.url = "github:cachix/nixpkgs-python";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = inputs @ { flake-parts, ... }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        { config
+        , self'
+        , inputs'
+        , pkgs
+        , system
+        , ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: { })
+            ];
+            config.allowUnfree = true;
+          };
+          devenv.shells.default = {
+            env = {
+              XLA_FLAGS = "--xla_gpu_cuda_data_dir=${pkgs.cudaPackages_11_8.cudatoolkit}"; # For tensorflow with GPU support
+              # https://github.com/google/REAPER/issues/14#issuecomment-651647572
+              LD_PRELOAD = "${pkgs.gperftools}/lib/libtcmalloc.so";
+            };
+            packages = with pkgs; [
+              cudaPackages_11_8.cudatoolkit
+              cudaPackages_11_8.cudnn_8_9
+              ffmpeg
+              file
+              glibc
+              gnumake
+              libgcc
+              llvmPackages_10.llvm
+              mecab
+              openssl
+              zlib
+            ];
+            languages = {
+              python = {
+                enable = true;
+                # poetry.enable = true;
+                # venv.enable = true;
+                version = "3.10";
+              };
+            };
+          };
+        };
+    };
+}
+
